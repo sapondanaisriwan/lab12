a703a88d874c5da0c20d67f1a6235f4d28e3f9f0
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2024-12-24 14:50:04 +0700

    .

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+long long int fibonacci(long long int);
+
+int main()
+{
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+long long int fibonacci(long long int x)
+{
+    if (x == 0)
+    {
+        return 0;
+    }
+    else if (x == 1)
+    {
+        return 1;
+    }
+    return fibonacci(x - 1) + fibonacci(x - 2);
 }
--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int gcd(int, int);
+
+int main()
+{
     int x, y;
     cin >> x >> y;
-    cout << gcd(x,y);
-    return 0; 
+    cout << gcd(x, y);
+    return 0;
+}
+
+int gcd(int x, int y)
+{
+    if (x % y == 0)
+    {
+        return y;
+    }
+    return gcd(y, x % y);
 }
